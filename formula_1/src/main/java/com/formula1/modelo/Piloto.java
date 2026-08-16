11187b7cbc033ebbc11923b1aa6e50763d0dd30d
juliand06 <andreyricaurte10@gmail.com>
2026-08-16 12:21:55 -0500

    Agregada clase Piloto.java

---
+++ formula_1/src/main/java/com/formula1/modelo/Piloto.java
@@ -0,0 +1,25 @@
+package com.formula1.modelo;
+
+public class Piloto {
+    private int id;
+    private String nombre;
+    private String equipo;
+    private String rol;
+    private int experienciaAnios;
+    private int habilidad;
+
+    public Piloto() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getEquipo() { return equipo; }
+    public void setEquipo(String equipo) { this.equipo = equipo; }
+    public String getRol() { return rol; }
+    public void setRol(String rol) { this.rol = rol; }
+    public int getExperienciaAnios() { return experienciaAnios; }
+    public void setExperienciaAnios(int experienciaAnios) { this.experienciaAnios = experienciaAnios; }
+    public int getHabilidad() { return habilidad; }
+    public void setHabilidad(int habilidad) { this.habilidad = habilidad; }
+}
