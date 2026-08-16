7adaed2361b06f8f2cfd6d94a07a73fda57da774
juliand06 <andreyricaurte10@gmail.com>
2026-08-16 13:02:25 -0500

    agrega clase configuracion

---
+++ formula_1/src/main/java/com/formula1/modelo/ConfiguracionVehiculo.java
@@ -0,0 +1,22 @@
+package com.formula1.modelo;
+
+public class ConfiguracionVehiculo {
+    private String modeloVehiculo;
+    private String modoConduccion;
+    private double cargaAerodinamica;
+    private double presionNeumaticos;
+    private String estrategiaCombustible;
+
+    public ConfiguracionVehiculo() {}
+
+    public String getModeloVehiculo() { return modeloVehiculo; }
+    public void setModeloVehiculo(String modeloVehiculo) { this.modeloVehiculo = modeloVehiculo; }
+    public String getModoConduccion() { return modoConduccion; }
+    public void setModoConduccion(String modoConduccion) { this.modoConduccion = modoConduccion; }
+    public double getCargaAerodinamica() { return cargaAerodinamica; }
+    public void setCargaAerodinamica(double cargaAerodinamica) { this.cargaAerodinamica = cargaAerodinamica; }
+    public double getPresionNeumaticos() { return presionNeumaticos; }
+    public void setPresionNeumaticos(double presionNeumaticos) { this.presionNeumaticos = presionNeumaticos; }
+    public String getEstrategiaCombustible() { return estrategiaCombustible; }
+    public void setEstrategiaCombustible(String estrategiaCombustible) { this.estrategiaCombustible = estrategiaCombustible; }
+}
